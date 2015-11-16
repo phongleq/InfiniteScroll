be847ac5401012f935c4dc73f0cb86197bb47627
Le, Phong <phong.le@mirumagency.com>
2015-11-15 21:59:14 -0700

    initial

---
+++ InfiniteScroll/InfiniteScroll.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteScroll.h
+//  InfiniteScroll
+//
+//  Created by Le, Phong on 11/15/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteScroll.
+FOUNDATION_EXPORT double InfiniteScrollVersionNumber;
+
+//! Project version string for InfiniteScroll.
+FOUNDATION_EXPORT const unsigned char InfiniteScrollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteScroll/PublicHeader.h>
+
+
